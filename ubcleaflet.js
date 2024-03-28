70c1967d27312cda407941709b5c64250e9ce4a3
fschute <156774371+fschute@users.noreply.github.com>
2024-03-28 00:19:22 -0700

    Update ubcleaflet.js

--- ubcleaflet.js
+++ ubcleaflet.js
@@ -1,6 +1,40 @@
-var map = L.map('mapid', 
-                {
-                  center: [49.265637, -123.256113], 
-                  zoom: 15
-                }
-); 
+      var map = L.map('mapid',
+                        {
+          				  center: [49.265637, -123.256113],
+          				  zoom: 15
+        				} 
+                  );
+
+      var topoTiles = L.tileLayer(
+          'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+                          {
+      						maxZoom: 17,
+      						attribution: 'Basemap data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Basemap style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+      					  }
+      					).addTo(map);
+
+        function onEachPoint(feature, layer) {
+           if (feature.properties && feature.properties.BLDG_CODE) {
+              layer.bindTooltip( feature.properties.BLDG_CODE);
+           }
+        }
+
+      $.getJSON("https://raw.githubusercontent.com/UBCGeodata/ubc-geospatial-opendata/master/ubcv/locations/geojson/ubcv_buildings.geojson",
+                
+        function(data){
+        	L.geoJSON(data, {
+       			style: function (feature, latlng) {},
+                onEachFeature: onEachPoint}).addTo(map);
+            L.geoJson(data, {style: style}).addTo(map);
+            });
+      
+      
+        function style(feature) {
+            return {
+            fillColor: "#87ABB7",
+            weight: 2,
+            opacity: 1,
+            color: '#CECECE',
+            fillOpacity: 0.8
+    };
+}
